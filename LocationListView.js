2712b5136626ef3cdf4fd2e3cdf89e167732f446
Tim Pryor <tdpryor@gmail.com>
2016-07-14 16:48:36 -0400

    Can see static city names and seperator between the cells.

--- LocationListView.js
+++ LocationListView.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React, {Component} from 'react';
-import { AppRegistry, ListView, Text, View } from "react-native";
+import { AppRegistry, ListView, Text, View, TouchableHighlight } from "react-native";
 
 
 class LocationListView extends Component {
@@ -11,16 +11,39 @@ class LocationListView extends Component {
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== rs});
         this.state = {
             dataSource: ds.cloneWithRows([
-                'Montreal', 'Laval', 'Toronto', 'Quebec City', 'Ottawa'
+                'Montreal', 'Oakville', 'Toronto', 'Quebec City', 'Ottawa'
             ])
         }
     }
+
+    renderRow(rowData) {
+        return (
+            <View style={{
+                flex: 1,
+                flexDirection: 'row',
+                padding: 20,
+                alignItems: 'center',
+                borderColor: '#D7D7D7',
+                borderBottomWidth: 1
+            }}>
+            <Text style={{fontSize: 45}}> {rowData} </Text>
+            </View>
+        );
+    }
+
+    renderSeperator(sectionID, rowID) {
+        return (
+            <View style={{backgroundColor:'#CCCCCC'}} />
+        )
+    }
+
     render() {
         return (
             <View style={{paddingTop: 22}}>
                 <ListView
                     dataSource={this.state.dataSource}
-                    renderRow={(rowData) => <Text>{rowData}</Text>}
+                    renderRow={this.renderRow}
+                    renderSeperator={this.renderSeparator}
                 />
             </View>
         )
--- index.ios.js
+++ index.ios.js
@@ -18,18 +18,6 @@ class RNWeather extends Component {
   render() {
     return (
         <LocationListView />
-    //   <View style={styles.container}>
-    //     <Text style={styles.welcome}>
-    //       Welcome to React Native!
-    //     </Text>
-    //     <Text style={styles.instructions}>
-    //       To get started, edit index.ios.js
-    //     </Text>
-    //     <Text style={styles.instructions}>
-    //       Press Cmd+R to reload,{'\n'}
-    //       Cmd+D or shake for dev menu
-    //     </Text>
-    //   </View>
     );
   }
 }
