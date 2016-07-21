f8d87249cf9e64d1ca4f1fbd544d9f209684590e
Tim Pryor <tdpryor@gmail.com>
2016-07-21 14:40:28 -0400

    Created new array of objects for east and west locations. Each object contains a name and image property.

---
+++ ios/SortCities.h
@@ -0,0 +1,13 @@
+//
+//  SortCities.h
+//  RNWeather
+//
+//  Created by Tim Pryor on 2016-07-21.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#ifndef SortCities_h
+#define SortCities_h
+
+
+#endif /* SortCities_h */
---
+++ ios/SortCities.m
@@ -0,0 +1,9 @@
+//
+//  SortCities.m
+//  RNWeather
+//
+//  Created by Tim Pryor on 2016-07-21.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
